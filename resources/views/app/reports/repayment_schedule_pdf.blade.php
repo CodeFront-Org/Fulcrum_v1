7fbda681c55ba0c2d1e2440b5b69518fec4b846a
thekuuri <nganga.n.kuuri@gmail.com>
2025-10-29 16:19:09 +0300

    Add loan ID and update payment periods in repayment schedule PDF

--- resources/views/app/reports/repayment_schedule_pdf.blade.php
+++ resources/views/app/reports/repayment_schedule_pdf.blade.php
@@ -17,6 +17,7 @@
     <table>
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Customer Name</th>
                 <th>Loan Principal</th>
                 <th>Disbursed Date</th>
@@ -28,10 +29,36 @@
         <tbody>
             @foreach($loans as $loan)
             <tr>
+                <td>{{ $loan->id}}</td>
                 <td>{{ $loan->user->first_name }} {{ $loan->user->last_name }}</td>
                 <td class="amount">{{ number_format($loan->requested_loan_amount, 2) }}</td>
                 <td>{{ $loan->created_at->format('Y-m-d') }}</td>
-                <td>{{ $loan->interest_rate }}</td>
+              
+
+                {{--add the company model --}}
+          
+                    @php
+                        $company = \App\Models\Company::find($loan->company_id);
+
+                    // Store month fields that are not null
+                    $periods = [
+                        0,
+                        $company->month1,
+                        $company->month2,
+                        $company->month3,
+                        $company->month4,
+                        $company->month5,
+                        $company->month6,
+                        $company->month7,
+                        $company->month8,
+                        $company->month9,
+                        $company->month10,
+                        $company->month11,
+                        $company->month12
+                       ];
+                @endphp
+               
+                 <td>{{ $periods[$loan->payment_period]*100  ?? 'N/A' }}%</td>
                 <td>{{ $loan->current_payment_period }} / {{ $loan->payment_period }}</td>
                 <td class="amount">{{ number_format($loan->amount_payable, 2) }}</td>
             </tr>
